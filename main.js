1c63f825f4609d239cc164e05f3729681b87d999
Tim Thuis <tim.thuis@gmail.com>
2017-07-31 14:27:16 +0200

    add product detail page cards

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 let played = false;
+let playedDetailPage = false;
 
 const cardBarShowDetails = new TimelineMax({
+  paused: true,
+});
+
+const detailPageShow = new TimelineMax({
   paused: true,
 });
 
@@ -20,8 +25,26 @@ cardBarShowDetails.to('#home-page', 0.5, {
   }, 'start+=0.5')
   .staggerFrom('.card-bar-small', 0.5, {
     opacity: 0,
-    y: '-100%'
+    y: '-100%',
+    ease: Back.easeOut.config(2),
   }, 0.25, 'start+=1')
+
+detailPageShow.to('#card-product-detail', 1, {
+  rotationY: '180deg',
+}, 'start')
+  .to('#home-page', 0.5, {
+    padding: 0,
+  }, 'start+=0.5')
+  .to('#card-product-detail .x', 0, {
+    opacity: 0,
+  }, 'start+=0.3')
+  .toFrom('#card-product-detail', 1, {
+    height: '100vh',
+    padding: '0'
+  }, 'start+=1')
+  .staggerFrom('#detail-page .card-main', 1, {
+    x: '-200%',
+  }, 0.5, 'start+=1')
 
 
 function startCardBarDetailShow(element) {
@@ -34,127 +57,12 @@ function startCardBarDetailShow(element) {
   }
 }
 
-// const dataFlowsTl = new TimelineMax();
-// const dataFlowChartShowTl = new TimelineMax({
-//   paused: true,
-// })
-// const detailPageTl = new TimelineMax({
-//   paused: true,
-// })
-// const showDetailCardTl = new TimelineMax({
-//   paused: true,
-// })
-//
-//
-// showDetailCardTl.to('.card-test .one', 1, {
-//   scale: 0.75,
-//   opacity: 0.75,
-// }, 'start')
-//   .to('.card-test .two', 1, {
-//     x: '0%',
-//   }, 'start+=0.5')
-//
-//
-//
-// detailPageTl.to('.flow', 1, {
-//   x: '-100%',
-// }, 'start')
-//   .to('.flow', 0.5, {
-//     height: 0,
-//     padding: 0,
-//   }, 'start+=1')
-//   .to('.card h2', 0.5, {
-//     y: '-150%',
-//     padding: 0,
-//   }, 'start')
-//   .to('.card h2', 0.5, {
-//     opacity: 0,
-//   }, 'start+=0.5')
-//   .staggerTo('.card', 0.5, {
-//     height: '200px',
-//     padding: 0,
-//     overflow: 'visible'
-//   }, 0.25, 'start+=1')
-//   .to('.card .slide-card', 0.5, {
-//     height: '100%',
-//   }, 'start+=1')
-//   .to('.title h1', 0.5, {
-//     y: 0
-//   }, 'start')
-//   .to('.fa-times', 0.5, {
-//     opacity: 1,
-//   }, 'start')
-//   .to('.card .card-detail', 0.5, {
-//     opacity: 1,
-//   }, 'start+=1.5')
-//   .from('.card .card-detail', 0.25, {
-//     scale: 0.9,
-//   }, 'start+=1.5')
-//
-// dataFlowsTl.staggerFrom('.card', 0.5, {
-//   x: '100%',
-// }, 0.1, 'start')
-//   .staggerFrom('.flow', 0.5, {
-//     width: 0,
-//   }, 0.1, 'start+=0.5')
-//   .staggerFrom('.flow .diff-status-bar', 0.5, {
-//     width: 0,
-//   }, 0.1, 'start+=1')
-//   .staggerFrom('.flow .percent', 0.25, {
-//     y: '-100%',
-//   }, 0.1, 'start+=1')
-//   .staggerFrom('.flow .grow', 0.25, {
-//     y: '-100%',
-//   }, 0.1, 'start+=1.25')
-//
-//
-// function showGraph(el) {
-//   const statusBar = el.querySelector('.diff-status-bar');
-//
-//   dataFlowChartShowTl.to(el, 0.5, {
-//     width: '100%',
-//   }, 'start')
-//     .to(statusBar, 0.5, {
-//       width: '0',
-//     }, 'start')
-//     .to('.numbers', 0.5, {
-//       marginTop: '16px',
-//     }, 'start+=0.5')
-//     .to('.graph', 0.5, {
-//       height: '200px',
-//     }, 'start+=0.5')
-//     .to('.flow', 0.5, {
-//       padding: 0,
-//     }, 'start+=0.5')
-//     .to('.graph', 1, {
-//       width: '100%',
-//     }, 'start+=1')
-//     .to('.more', 0.5, {
-//       y: '0%'
-//     }, 'start+=2')
-//
-//   if (el.classList.contains('open')) {
-//     el.classList.remove('open');
-//     dataFlowChartShowTl.reverse();
-//   } else {
-//     el.classList.add('open');
-//     dataFlowChartShowTl.play();
-//   }
-// }
-//
-// function showDetailPage() {
-//   detailPageTl.play();
-// }
-//
-// function hideDetailPage() {
-//   detailPageTl.reverse();
-// }
-//
-//
-// function slideCardPlay() {
-//   showDetailCardTl.play();
-// }
-//
-// function slideCardRev() {
-//   showDetailCardTl.reverse();
-// }
+function startDetailPageShow() {
+  if (!playedDetailPage) {
+    detailPageShow.play();
+    playedDetailPage = !playedDetailPage;
+  } else {
+    detailPageShow.reverse();
+    playedDetailPage = !playedDetailPage;
+  }
+}
